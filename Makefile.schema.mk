0a19f4f326baa6a13b3630fac19d95adb0a02205
Moritz Gottschling <moritz@giantswarm.io>
2023-02-23 10:09:21 +0100

    Add makefile to validate schema and generate values.yaml

---
+++ Makefile.schema.mk
@@ -0,0 +1,9 @@
+.PHONY: schema-validate
+schema-validate:
+	go install github.com/giantswarm/schemalint@latest
+	schemalint verify ./helm/cluster-azure/values.schema.json --rule-set=cluster-app
+
+.PHONY: values-generate
+values-generate:
+	go install github.com/giantswarm/helm-values-gen@latest
+	helm-values-gen ./helm/cluster-azure/values.schema.json -o ./helm/cluster-azure/values.yaml --force
