60e6f837034efc9faba236f2cba33fa9ffd1b8e6
ArchitectBot <61872893+architectbot@users.noreply.github.com>
2023-04-25 02:45:35 -0400

    Align files (#117)
    Co-authored-by: github-actions <action@github.com>
    Co-authored-by: Mario Constanti <github@constanti.de>

--- Makefile.gen.cluster_app.mk
+++ Makefile.gen.cluster_app.mk
@@ -1,24 +1,25 @@
 # DO NOT EDIT. Generated with:
 #
-#    devctl@5.21.0
+#    devctl@5.22.0
 #
 
+##@ Schema
 
 VALUES=$(shell find ./helm -maxdepth 2 -name values.yaml)
 VALUES_SCHEMA=$(shell find ./helm -maxdepth 2 -name values.schema.json)
 
 .PHONY: normalize-schema
-normalize-schema:
+normalize-schema: ## Normalize the JSON schema
 	go install github.com/giantswarm/schemalint@v1
 	schemalint normalize $(VALUES_SCHEMA) -o $(VALUES_SCHEMA) --force
 
 .PHONY: validate-schema
-validate-schema:
+validate-schema: ## Validate the JSON schema
 	go install github.com/giantswarm/schemalint@v1
 	schemalint verify $(VALUES_SCHEMA) --rule-set=cluster-app
 
 .PHONY: generate-values
-generate-values:
+generate-values: ## Generate values.yaml from schema
 	go install github.com/giantswarm/helm-values-gen@v1
 	helm-values-gen $(VALUES_SCHEMA) -o $(VALUES) --force
 
